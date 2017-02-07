d8c62d886196b47388af4d016d5ae1048953a5cb
Joe Jarvis <joe.jarvis109@gmail.com>
2017-02-07 18:38:33 -0500

    Made some changes to options script

--- options.js
+++ options.js
@@ -1,16 +1,11 @@
 function saveOptions(e) {
-  browser.storage.local.set({
-    days: document.querySelector("#days").value
-  });
-  e.preventDefault();
+    browser.storage.local.set({days: document.querySelector("#days").value});
+    e.preventDefault();
 }
-
 function restoreOptions() {
-  var gettingItem = browser.storage.local.get('days');
-  gettingItem.then((res) => {
-    document.querySelector("#days").value = res.days || '0';
-  });
+    browser.storage.local.get('days', function(res) {
+        document.querySelector("#days").value = res.days || '0';
+    });
 }
-
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
