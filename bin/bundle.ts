4f40e539c6479792e3ef4e305932e4ba0e44e8b9
Joe Jarvis <joe.jarvis109@gmail.com>
2020-12-05 16:10:41 -0500

    Remove unneeded options from bundle script

--- bin/bundle.ts
+++ bin/bundle.ts
@@ -3,15 +3,9 @@ import Bundler from "parcel-bundler";
 const entryFiles = ["src/**/*.html", "src/background.ts"];
 
 const options = {
-    outDir: "./dist",
     watch: true,
-    cache: true,
-    cacheDir: ".cache",
-    minify: false,
     target: "browser",
-    logLevel: 3,
     hmr: false,
-    sourceMaps: true,
     detailedReport: false,
     autoInstall: false
 };
@@ -20,7 +14,6 @@ export async function bundle(production: boolean): Promise<void> {
     if (production) {
         options.watch = false;
         options.detailedReport = true;
-        options.minify = true;
     }
 
     const bundler = new Bundler(entryFiles, options);
