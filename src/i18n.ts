3133cf9f982be32549c32d62ff628f7b6db60055
Joe Jarvis <joe.jarvis109@gmail.com>
2020-12-22 17:47:35 -0500

    Docs for i18n function

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,12 @@
 import { browser } from "webextension-polyfill-ts";
 
+/**
+ * Loads i18n text to page
+ *
+ * Affects innerText of elements with class i18n
+ *
+ * Elements must have dataset.i18n present with the id of the i18n string
+ */
 export function i18n(): void {
     ([...document.getElementsByClassName("i18n")] as HTMLElement[])
         .forEach(item => {
